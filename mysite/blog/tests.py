3b48e9e4d9a54562258f47b098e5b49524db151d
BenBagBag <IsaacC@allenai.org>
2017-03-04 18:55:43 -0800

    list now working

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,14 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blog.models import Post, Category
+import datetime
+from django.utils.timezone import utc
 
 # Create your tests here.
 class PostTestCase(TestCase):
-    # fixtures = ['myblog_test_fixture.json', ]
-    #
-    # def setUp(self):
-    #     self.User = User.objects.get(pk=1)
-
     def test_string_representation(self):
         expected = "This is a title"
         p1 = Post(title=expected)
@@ -22,3 +19,31 @@ class CategoryTestCase(TestCase):
         c1 = Category(name=expected)
         actual = str(c1)
         self.assertEqual(expected, actual)
+
+
+class FrontEndTestCase(TestCase):
+    fixtures = ['blog_test_fixture.json', ]
+    #
+    def setUp(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+        author = User.objects.get(pk=1)
+        for count in range(1, 11):
+            post = Post(title="Post %d Title" % count,
+                        text="foo",
+                        author=author)
+            if count < 6:
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+        resp_text = resp.content.decode(resp.charset)
+        self.assertTrue("Recent Posts" in resp_text)
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            if count < 6:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
