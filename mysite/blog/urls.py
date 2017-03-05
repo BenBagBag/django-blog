da891479239175181b88dc5ae885bab947eb9f54
BenBagBag <IsaacC@allenai.org>
2017-03-04 18:59:56 -0800

    detail view added

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-from blog.views import list_view, stub_view
+from blog.views import detail_view, list_view, stub_view
 
 urlpatterns = [
     url(r'^$',
         list_view,
         name="blog_index"),
     url(r'^posts/(?P<post_id>\d+)/$',
-    stub_view,
+    detail_view,
     name="blog_detail"),
 ]
