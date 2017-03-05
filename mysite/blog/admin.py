fd840e5425efd6ce8fde1229580c3f68e80748cb
BenBagBag <IsaacC@allenai.org>
2017-03-04 19:41:34 -0800

    clean up category editing

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from blog.models import Category, Post
 
+class CategoryInline(admin.TabularInline):
+    model = Post.categories.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('title', 'published_date')
+    inlines = [CategoryInline, ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
